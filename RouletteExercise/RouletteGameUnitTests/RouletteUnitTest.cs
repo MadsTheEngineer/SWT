3ef38bfa5ce33d90f4bcb64fe8a1101478283c66
Casper Andersen <repsac10@msn.com>
2014-09-22 09:56:31 +0200

    IRandomize added RouletteUnitTest added

---
+++ RouletteExercise/RouletteGameUnitTests/RouletteUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RouletteGame;
+
+namespace RouletteGameUnitTests
+{
+    [TestFixture]
+    public class RouletteUnitTest
+    {
+        private Roulette _uut;
+        [SetUp]
+        public void RunBeforeTests()
+        {
+            _uut = new Roulette();
+        }
+
+        [Test]
+        public void Constructor_ResultIsFieldZero_FieldNumberIsZero()
+        {
+            var number =_uut.GetResult().Number;
+            Assert.That(number,Is.EqualTo(0));
+        }
+
+        // Laver en fake af 
+    }
+}
