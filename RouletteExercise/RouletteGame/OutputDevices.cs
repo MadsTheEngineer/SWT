de08c61f34bbad80211ea437e08d23210823c958
Jonas Hitomi Balslev Ishibori <20013575@post.au.dk>
2014-09-22 09:44:33 +0200

    OutputDevices.cs with IOutputDevice interface and consoleDevice

---
+++ RouletteExercise/RouletteGame/OutputDevices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouletteGame
+{
+    public interface IOutputDevice
+    {
+        void Render(string s);
+    }
+
+    public class ConsoleDevice
+    {
+        public void Render(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
